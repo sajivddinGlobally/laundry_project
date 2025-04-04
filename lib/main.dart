767806d8ea9d7c7eb4e39fc429d6ad28a8ecdef4
sajivddin <sajivddin.websolutions@gmail.com>
2025-04-04 17:21:03 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -38,8 +38,8 @@ class MyApp extends StatelessWidget {
             // tested with just a hot reload.
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           ),
-          // home: LandingPage(),
-          home: StaffPage(),
+          home: LandingPage(),
+          // home: StaffPage(),
         );
       },
     );
